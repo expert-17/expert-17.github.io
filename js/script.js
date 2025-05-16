db8ee763e2a6eae7aa56549b88800a972583793b
Luyolo <44893424+yolopalisson@users.noreply.github.com>
2025-05-16 19:17:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,14 @@ $('#toggle').click(function() {
     document.getElementById("imageModal").style.display = "none";
   }
 
+  document.addEventListener('DOMContentLoaded', () => {
+   const track = document.querySelector('.carousel-track');
+   const logos = Array.from(track.children);
+   const logoWidth = logos[0].getBoundingClientRect().width;
+
+   // Clone logos to create a seamless loop
+   logos.forEach(logo => {
+       const clone = logo.cloneNode(true);
+       track.appendChild(clone);
+   });
+});
