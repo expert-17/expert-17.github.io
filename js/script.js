279b031188d91dfebbf61ba69bf48bc403d8dab7
Luyolo <44893424+yolopalisson@users.noreply.github.com>
2025-05-16 19:06:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+  function openModal(img) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  }
+
+  function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+  }
+
